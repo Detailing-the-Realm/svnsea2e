2c3f38450b41d65c5e5bc1685e11f033d07c1884
Jason Hardin <jason@jasonhardin.com>
2021-03-19 22:12:22 -0400

    release

--- module/migration.js
+++ module/migration.js
@@ -2,15 +2,19 @@
  * Perform a system migration for the entire World, applying migrations for Actors, Items, and Compendium packs
  * @return {Promise}      A Promise which resolves once the migration is completed
  */
-export const migrateWorld = async function () {
-  ui.notifications.info(`Applying 7th Sea 2E System Migration for version ${game.system.data.version}. Please be patient and do not close your game or shut down your server.`, { permanent: true })
+export const migrateWorld = async function() {
+  ui.notifications.info(`Applying 7th Sea 2E System Migration for version ${game.system.data.version}. Please be patient and do not close your game or shut down your server.`, {
+    permanent: true
+  })
   // Migrate World Actors
   for (const a of game.actors.entities) {
     try {
       const updateData = migrateActorData(a.data)
       if (!isObjectEmpty(updateData)) {
         console.log(`Migrating Actor entity ${a.name}`)
-        await a.update(updateData, { enforceTypes: false })
+        await a.update(updateData, {
+          enforceTypes: false
+        })
       }
     } catch (err) {
       console.error(err)
@@ -23,7 +27,9 @@ export const migrateWorld = async function () {
       const updateData = migrateItemData(i.data)
       if (!isObjectEmpty(updateData)) {
         console.log(`Migrating Item entity ${i.name}`)
-        await i.update(updateData, { enforceTypes: false })
+        await i.update(updateData, {
+          enforceTypes: false
+        })
       }
     } catch (err) {
       console.error(err)
@@ -34,13 +40,15 @@ export const migrateWorld = async function () {
   const packs = game.packs.filter(p => {
     return (p.metadata.package === "world") && ["Actor", "Item", "Scene"].includes(p.metadata.entity)
   })
-  for ( let p of packs ) {
+  for (let p of packs) {
     await migrateCompendium(p)
   }
 
   // Set the migration as complete
   game.settings.set('svnsea2e', 'systemMigrationVersion', game.system.data.version)
-  ui.notifications.info(`7th Sea 2E System Migration to version ${game.system.data.version} completed!`, { permanent: true })
+  ui.notifications.info(`7th Sea 2E System Migration to version ${game.system.data.version} completed!`, {
+    permanent: true
+  })
 }
 
 /* -------------------------------------------- */
@@ -54,26 +62,26 @@ export const migrateWorld = async function () {
  */
 export const migrateCompendium = async function(pack) {
   const entity = pack.metadata.entity
-  if ( !["Actor", "Item", "Scene"].includes(entity) ) return
+  if (!["Actor", "Item", "Scene"].includes(entity)) return
 
   // Begin by requesting server-side data model migration and get the migrated content
   await pack.migrate();
   const content = await pack.getContent();
 
   // Iterate over compendium entries - applying fine-tuned migration functions
-  for ( let ent of content ) {
+  for (let ent of content) {
     try {
       let updateData = null
       if (entity === "Item") updateData = migrateItemData(ent.data)
       else if (entity === "Actor") updateData = migrateActorData(ent.data)
-      else if ( entity === "Scene" ) updateData = migrateSceneData(ent.data)
+      else if (entity === "Scene") updateData = migrateSceneData(ent.data)
       if (!isObjectEmpty(updateData)) {
         expandObject(updateData);
         updateData["_id"] = ent._id
         await pack.updateEntity(updateData)
         console.log(`Migrated ${entity} entity ${ent.name} in Compendium ${pack.collection}`)
       }
-    } catch(err) {
+    } catch (err) {
       console.error(err)
     }
   }
@@ -86,10 +94,10 @@ export const migrateCompendium = async function(pack) {
  * @param {Actor} actor   The actor to Update
  * @return {Object}       The updateData to apply
  */
-export const migrateActorData = function (actor) {
+export const migrateActorData = function(actor) {
   const updateData = {}
 
-  if (actor.type === 'playercharacter' && actor.data.wealth == null){
+  if (actor.type === 'playercharacter' && actor.data.wealth == null) {
     updateData['data.wealth'] = 0
   }
 
@@ -111,18 +119,23 @@ export const migrateActorData = function (actor) {
     updateData['data.traits.strength.max'] = 20
   }
 
-  if(actor.type === 'dangerpts' && actor.data.points < 5) {
+  if (actor.type === 'dangerpts' && actor.data.points < 5) {
     updateData['data.points'] = 5
   }
 
-  if(actor.type === 'monster' && actor.data.fear.max != 5) {
+  if (actor.type === 'monster' && actor.data.fear.max != 5) {
     updateData['data.fear.value'] = 0
     updateData['data.fear.min'] = 0
     updateData['data.fear.max'] = 5
   }
 
-  if(actor.type === 'ship' && actor.data.crewstatus == null ) {
-    updateData['data.crewstatus'] = ""
+  if (actor.type === 'ship' && actor.data.crewstatus == null) {
+    if (actor.data.crewstatus == null) {
+      updateData['data.crewstatus'] = ""
+    }
+    if (aactor.data.wealth == null) {
+      updateData['data.wealth'] = "0"
+    }
   }
 
   return updateData
@@ -134,7 +147,7 @@ export const migrateActorData = function (actor) {
  * @param {Actor} actor   The actor to Update
  * @return {Object}       The updateData to apply
  */
-export const migrateItemData = function (item) {
+export const migrateItemData = function(item) {
   const updateData = {}
 
   if (item.type === 'secretsociety' && typeof item.favor === 'undefined') {
--- module/svnsea2e.js
+++ module/svnsea2e.js
@@ -1,35 +1,81 @@
 // Import Modules
-import { SVNSEA2E } from './config.js'
-import { preloadHandlebarsTemplates } from './templates.js'
-import { registerSystemSettings } from './settings.js'
+import {
+  SVNSEA2E
+} from './config.js'
+import {
+  preloadHandlebarsTemplates
+} from './templates.js'
+import {
+  registerSystemSettings
+} from './settings.js'
 
 // Import Applications
-import { SvnSea2EActor } from './actor/actor.js'
-import { ActorSheetSS2ePlayerCharacter } from './actor/sheets/playercharacter.js'
-import { ActorSheetSS2eHero } from './actor/sheets/hero.js'
-import { ActorSheetSS2eBrute } from './actor/sheets/brute.js'
-import { ActorSheetSS2eMonster } from './actor/sheets/monster.js'
-import { ActorSheetSS2eVillain } from './actor/sheets/villain.js'
-import { ActorSheetSS2eShip } from './actor/sheets/ship.js'
-import { ActorSheetSS2eDangerPts } from './actor/sheets/dangerpts.js'
-import { SvnSea2EItem } from './item/item.js'
-import { ItemSheetSS2eAdvantage } from './item/sheets/advantage.js'
-import { ItemSheetSS2eArtifact } from './item/sheets/artifact.js'
-import { ItemSheetSS2eBackground } from './item/sheets/background.js'
-import { ItemSheetSS2eDuelStyle } from './item/sheets/duelstyle.js'
-import { ItemSheetSS2eMonsterQuality } from './item/sheets/monsterquality.js'
-import { ItemSheetSS2eSecretSociety } from './item/sheets/secretsociety.js'
-import { ItemSheetSS2eScheme } from './item/sheets/scheme.js'
-import { ItemSheetSS2eShipAdventure } from './item/sheets/shipadventure.js'
-import { ItemSheetSS2eShipBackground } from './item/sheets/shipbackground.js'
-import { ItemSheetSS2eSorcery } from './item/sheets/sorcery.js'
-import { ItemSheetSS2eStory } from './item/sheets/story.js'
+import {
+  SvnSea2EActor
+} from './actor/actor.js'
+import {
+  ActorSheetSS2ePlayerCharacter
+} from './actor/sheets/playercharacter.js'
+import {
+  ActorSheetSS2eHero
+} from './actor/sheets/hero.js'
+import {
+  ActorSheetSS2eBrute
+} from './actor/sheets/brute.js'
+import {
+  ActorSheetSS2eMonster
+} from './actor/sheets/monster.js'
+import {
+  ActorSheetSS2eVillain
+} from './actor/sheets/villain.js'
+import {
+  ActorSheetSS2eShip
+} from './actor/sheets/ship.js'
+import {
+  ActorSheetSS2eDangerPts
+} from './actor/sheets/dangerpts.js'
+import {
+  SvnSea2EItem
+} from './item/item.js'
+import {
+  ItemSheetSS2eAdvantage
+} from './item/sheets/advantage.js'
+import {
+  ItemSheetSS2eArtifact
+} from './item/sheets/artifact.js'
+import {
+  ItemSheetSS2eBackground
+} from './item/sheets/background.js'
+import {
+  ItemSheetSS2eDuelStyle
+} from './item/sheets/duelstyle.js'
+import {
+  ItemSheetSS2eMonsterQuality
+} from './item/sheets/monsterquality.js'
+import {
+  ItemSheetSS2eSecretSociety
+} from './item/sheets/secretsociety.js'
+import {
+  ItemSheetSS2eScheme
+} from './item/sheets/scheme.js'
+import {
+  ItemSheetSS2eShipAdventure
+} from './item/sheets/shipadventure.js'
+import {
+  ItemSheetSS2eShipBackground
+} from './item/sheets/shipbackground.js'
+import {
+  ItemSheetSS2eSorcery
+} from './item/sheets/sorcery.js'
+import {
+  ItemSheetSS2eStory
+} from './item/sheets/story.js'
 import LanguageSelector from './apps/language-selector.js'
 import SkillSelector from './apps/skill-selector.js'
 
 import * as migrations from './migration.js'
 
-Hooks.once('init', async function () {
+Hooks.once('init', async function() {
   console.log(`7th Sea 2E | Initializing 7th Sea Second Edition System
     ${SVNSEA2E.ASCII}`)
   game.svnsea2e = {
@@ -63,30 +109,84 @@ Hooks.once('init', async function () {
 
   // Register sheet application classes
   Actors.unregisterSheet('core', ActorSheet)
-  Actors.registerSheet('svnsea2e', ActorSheetSS2ePlayerCharacter, { types: ['playercharacter'], makeDefault: true })
-  Actors.registerSheet('svnsea2e', ActorSheetSS2eHero, { types: ['hero'], makeDefault: true })
-  Actors.registerSheet('svnsea2e', ActorSheetSS2eBrute, { types: ['brute'], makeDefault: true })
-  Actors.registerSheet('svnsea2e', ActorSheetSS2eMonster, { types: ['monster'], makeDefault: true })
-  Actors.registerSheet('svnsea2e', ActorSheetSS2eVillain, { types: ['villain'], makeDefault: true })
-  Actors.registerSheet('svnsea2e', ActorSheetSS2eShip, { types: ['ship'], makeDefault: true })
-  Actors.registerSheet('svnsea2e', ActorSheetSS2eDangerPts, { types: ['dangerpts'], makeDefault: true })
+  Actors.registerSheet('svnsea2e', ActorSheetSS2ePlayerCharacter, {
+    types: ['playercharacter'],
+    makeDefault: true
+  })
+  Actors.registerSheet('svnsea2e', ActorSheetSS2eHero, {
+    types: ['hero'],
+    makeDefault: true
+  })
+  Actors.registerSheet('svnsea2e', ActorSheetSS2eBrute, {
+    types: ['brute'],
+    makeDefault: true
+  })
+  Actors.registerSheet('svnsea2e', ActorSheetSS2eMonster, {
+    types: ['monster'],
+    makeDefault: true
+  })
+  Actors.registerSheet('svnsea2e', ActorSheetSS2eVillain, {
+    types: ['villain'],
+    makeDefault: true
+  })
+  Actors.registerSheet('svnsea2e', ActorSheetSS2eShip, {
+    types: ['ship'],
+    makeDefault: true
+  })
+  Actors.registerSheet('svnsea2e', ActorSheetSS2eDangerPts, {
+    types: ['dangerpts'],
+    makeDefault: true
+  })
 
   Items.unregisterSheet('core', ItemSheet)
-  Items.registerSheet('svnsea2e', ItemSheetSS2eAdvantage, { types: ['advantage'], makeDefault: true })
-  Items.registerSheet('svnsea2e', ItemSheetSS2eArtifact, { types: ['artifact'], makeDefault: true })
-  Items.registerSheet('svnsea2e', ItemSheetSS2eBackground, { types: ['background'], makeDefault: true })
-  Items.registerSheet('svnsea2e', ItemSheetSS2eDuelStyle, { types: ['duelstyle'], makeDefault: true })
-  Items.registerSheet('svnsea2e', ItemSheetSS2eMonsterQuality, { types: ['monsterquality'], makeDefault: true })
-  Items.registerSheet('svnsea2e', ItemSheetSS2eScheme, { types: ['scheme'], makeDefault: true })
-  Items.registerSheet('svnsea2e', ItemSheetSS2eSecretSociety, { types: ['secretsociety'], makeDefault: true })
-  Items.registerSheet('svnsea2e', ItemSheetSS2eShipAdventure, { types: ['shipadventure'], makeDefault: true })
-  Items.registerSheet('svnsea2e', ItemSheetSS2eShipBackground, { types: ['shipbackground'], makeDefault: true })
-  Items.registerSheet('svnsea2e', ItemSheetSS2eSorcery, { types: ['sorcery'], makeDefault: true })
-  Items.registerSheet('svnsea2e', ItemSheetSS2eStory, { types: ['story'], makeDefault: true })
+  Items.registerSheet('svnsea2e', ItemSheetSS2eAdvantage, {
+    types: ['advantage'],
+    makeDefault: true
+  })
+  Items.registerSheet('svnsea2e', ItemSheetSS2eArtifact, {
+    types: ['artifact'],
+    makeDefault: true
+  })
+  Items.registerSheet('svnsea2e', ItemSheetSS2eBackground, {
+    types: ['background'],
+    makeDefault: true
+  })
+  Items.registerSheet('svnsea2e', ItemSheetSS2eDuelStyle, {
+    types: ['duelstyle'],
+    makeDefault: true
+  })
+  Items.registerSheet('svnsea2e', ItemSheetSS2eMonsterQuality, {
+    types: ['monsterquality'],
+    makeDefault: true
+  })
+  Items.registerSheet('svnsea2e', ItemSheetSS2eScheme, {
+    types: ['scheme'],
+    makeDefault: true
+  })
+  Items.registerSheet('svnsea2e', ItemSheetSS2eSecretSociety, {
+    types: ['secretsociety'],
+    makeDefault: true
+  })
+  Items.registerSheet('svnsea2e', ItemSheetSS2eShipAdventure, {
+    types: ['shipadventure'],
+    makeDefault: true
+  })
+  Items.registerSheet('svnsea2e', ItemSheetSS2eShipBackground, {
+    types: ['shipbackground'],
+    makeDefault: true
+  })
+  Items.registerSheet('svnsea2e', ItemSheetSS2eSorcery, {
+    types: ['sorcery'],
+    makeDefault: true
+  })
+  Items.registerSheet('svnsea2e', ItemSheetSS2eStory, {
+    types: ['story'],
+    makeDefault: true
+  })
 
 
   // If you need to add Handlebars helpers, here are a few useful examples:
-  Handlebars.registerHelper('concat', function () {
+  Handlebars.registerHelper('concat', function() {
     var outStr = ''
     for (var arg in arguments) {
       if (typeof arguments[arg] !== 'object') {
@@ -96,11 +196,11 @@ Hooks.once('init', async function () {
     return outStr
   })
 
-  Handlebars.registerHelper('toLowerCase', function (str) {
+  Handlebars.registerHelper('toLowerCase', function(str) {
     return str.toLowerCase()
   })
 
-  Handlebars.registerHelper('capitalize', function (str) {
+  Handlebars.registerHelper('capitalize', function(str) {
     return str.charAt(0).toUpperCase() + str.slice(1)
   })
 
@@ -108,48 +208,48 @@ Hooks.once('init', async function () {
     var accum = '';
 
     const count = parseInt(from) + parseInt(to)
-    for(var i = from; i < count; i+=incr) {
-        block.data.index = i;
-        block.data.first = i === 0;
-        block.data.last = i === to;
-        block.data.mod = Math.trunc(i / 5)
-        block.data.remain = i % 5
-        accum += block.fn(this);
+    for (var i = from; i < count; i += incr) {
+      block.data.index = i;
+      block.data.first = i === 0;
+      block.data.last = i === to;
+      block.data.mod = Math.trunc(i / 5)
+      block.data.remain = i % 5
+      accum += block.fn(this);
     }
     return accum;
-});
+  });
 
   Handlebars.registerHelper('iff', function(a, operator, b, opts) {
     var bool = false
-    switch(operator) {
-       case '==':
-           bool = a == b
-           break
-       case '!=':
-           bool = a != b
-           break
-       case '>=':
-           bool = a >= b
-           break
-       case '<=':
-           bool = a <= b
-           break
-       case '>':
-           bool = a > b
-           break
-       case '<':
-           bool = a < b
-           break
-       default:
-           throw "Unknown operator " + operator
+    switch (operator) {
+      case '==':
+        bool = a == b
+        break
+      case '!=':
+        bool = a != b
+        break
+      case '>=':
+        bool = a >= b
+        break
+      case '<=':
+        bool = a <= b
+        break
+      case '>':
+        bool = a > b
+        break
+      case '<':
+        bool = a < b
+        break
+      default:
+        throw "Unknown operator " + operator
     }
 
     if (bool) {
-        return opts.fn(this)
+      return opts.fn(this)
     } else {
-        return opts.inverse(this)
+      return opts.inverse(this)
     }
-})
+  })
 
   // Preload Handlebars Templates
   preloadHandlebarsTemplates()
@@ -157,7 +257,7 @@ Hooks.once('init', async function () {
 
 /* -------------------------------------------- */
 
-Hooks.once('ready', async function () {
+Hooks.once('ready', async function() {
   game.svnsea2e.packAdvs = await getAllPackAdvantages()
   console.log('7th Sea 2E | Loaded Compendium Advantages')
   // Wait to register hotbar drop hook on ready so that
@@ -167,7 +267,7 @@ Hooks.once('ready', async function () {
   if (!currentVersion) {
     currentVersion = 0.6
   }
-  const NEEDS_MIGRATION_VERSION = 2.4
+  const NEEDS_MIGRATION_VERSION = 2.6
   // const COMPATIBLE_MIGRATION_VERSION = 0.6
   const needMigration = (currentVersion < NEEDS_MIGRATION_VERSION)
 
@@ -183,7 +283,7 @@ Hooks.once('ready', async function () {
  * This function runs after game data has been requested
  * and loaded from the servers, so entities exist
  */
-Hooks.once('setup', function () {
+Hooks.once('setup', function() {
   // Localize CONFIG objects once up-front
   const toLocalize = [
     'actorTypes',
@@ -204,8 +304,7 @@ Hooks.once('setup', function () {
   ]
 
   // Exclude some from sorting where the default order matters
-  const noSort = [
-  ]
+  const noSort = []
 
   // Localize and sort CONFIG objects
   for (const o of toLocalize) {
@@ -223,25 +322,25 @@ Hooks.once('setup', function () {
 /* -------------------------------------------- */
 
 /**
-* Set the default image for an item type instead of the mystery man
-**/
-Hooks.on('preCreateItem', function (entity, options, userId) {
+ * Set the default image for an item type instead of the mystery man
+ **/
+Hooks.on('preCreateItem', function(entity, options, userId) {
   entity.img = 'systems/svnsea2e/icons/' + entity.type + '.jpg'
 })
 
 /* -------------------------------------------- */
 
 /**
-* Set the default image for an actor type instead of the mystery man
-**/
-Hooks.on('preCreateActor', function (entity, options, userId) {
+ * Set the default image for an actor type instead of the mystery man
+ **/
+Hooks.on('preCreateActor', function(entity, options, userId) {
   entity.img = 'systems/svnsea2e/icons/' + entity.type + '.jpg'
   if (entity.name == "") {
     entity.name = "New " + (entity.type)[0].toUpperCase() + (entity.type).slice(1);
   }
 })
 
-async function getAllPackAdvantages () {
+async function getAllPackAdvantages() {
   const advantages = []
   const packs = game.packs.entries
   for (var i = 0; i < packs.length; i++) {
