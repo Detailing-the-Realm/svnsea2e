6fac2964184462deb6f846e7600b9c6684b4f833
Armaklan <lionel.zuber@armaklan.org>
2022-06-15 13:59:05 +0200

    feat: sheet relooking

--- module/combat.js
+++ module/combat.js
@@ -2,29 +2,28 @@ export function updateInitiative(actorId, raise) {
   const nRaise = parseFloat(raise);
   const activeCombat = game.combats.filter((combat) => combat.active);
 
-  if (activeCombat.length === 0) {
-    ui.notifications.error(`No combat is active`);
-    return;
+  if (activeCombat.length >= 0) {
+    activeCombat.forEach((combat) => {
+      const actors = combat.combatants.filter((c) => c.actor.id === actorId);
+
+      if (actors.length === 0) {
+        ui.notifications.error(
+          `This actor does not participate in this combat.`,
+        );
+        return;
+      }
+
+      combat.combatants
+        .filter((c) => c.actor.id === actorId)
+        .map((c) => {
+          c.update({ initiative: parseFloat(nRaise) });
+        });
+    });
   }
 
-  activeCombat.forEach((combat) => {
-    const actors = combat.combatants.filter((c) => c.actor.id === actorId);
-
-    if (actors.length === 0) {
-      ui.notifications.error(`This actor does not participate in this combat.`);
-      return;
-    }
-
-    combat.combatants
-      .filter((c) => c.actor.id === actorId)
-      .map((c) => {
-        c.update({ initiative: parseFloat(nRaise) });
-      });
-
-    game.actors
-      .filter((a) => a.id === actorId)
-      .map((a) => {
-        a.update({ data: { initiative: nRaise } });
-      });
-  });
+  game.actors
+    .filter((a) => a.id === actorId)
+    .map((a) => {
+      a.update({ data: { initiative: nRaise } });
+    });
 }
