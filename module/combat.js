3d7b3f85d26d5c25ae0764bc627ee620ebf881a4
Armaklan <lionel.zuber@armaklan.org>
2023-05-23 09:19:58 +0200

    fix: allow to use raise tracking without combat tracker

--- module/combat.js
+++ module/combat.js
@@ -2,22 +2,17 @@ export function updateInitiative(actorId, raise) {
   const nRaise = parseFloat(raise);
   const activeCombat = game.combats.filter((combat) => combat.active);
 
-  if (activeCombat.length >= 0) {
+  if (activeCombat.length > 0) {
     activeCombat.forEach((combat) => {
       const actors = combat.combatants.filter((c) => c.actor.id === actorId);
 
-      if (actors.length === 0) {
-        ui.notifications.error(
-          `This actor does not participate in this combat.`,
-        );
-        return;
+      if (actors.length > 0) {
+        combat.combatants
+          .filter((c) => c.actor.id === actorId)
+          .map((c) => {
+            c.update({ initiative: parseFloat(nRaise) });
+          });
       }
-
-      combat.combatants
-        .filter((c) => c.actor.id === actorId)
-        .map((c) => {
-          c.update({ initiative: parseFloat(nRaise) });
-        });
     });
   }
 
