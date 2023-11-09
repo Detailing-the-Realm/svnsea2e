d9a8a5341db1a1aa5309a14074fe435f4815b9af
Matt Raykowski <matt.raykowski@gmail.com>
2023-11-09 13:49:05 -0600

    Converted from gulp to Vite for bundling and development.

---
+++ tools/consts.mjs
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2021 Johannes Loher
+//
+// SPDX-License-Identifier: MIT
+
+export const name = "svnsea2e";
+export const sourceDirectory = "./src";
+export const distDirectory = "./dist";
+export const destinationDirectory = "systems";
+export const foundryconfigFile = "./foundryconfig.json";
